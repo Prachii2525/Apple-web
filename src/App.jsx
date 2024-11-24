56790da7029d31881025148eb3b5d21250e6527e
Prachiii18 <prachijanjire@gmail.com>
2024-11-24 22:06:13 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar1';
+
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+
+
+
+
+
+
+const App = () => {
+  
+
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Highlights.jsx
@@ -0,0 +1,10 @@
+const Highlights = () => {
+    return (
+        <div>
+            <h1>Highlights Section</h1>
+        </div>
+    );
+};
+
+export default Highlights;
+
